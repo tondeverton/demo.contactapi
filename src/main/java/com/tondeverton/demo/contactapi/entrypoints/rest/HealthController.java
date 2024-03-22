7793219fb098cae4711c17ff32efbeda3ebb4b40
Everton Pereira <me@ton.dev.br>
2024-03-22 22:59:24 +0000

    package entrypoint renamed to plural

--- src/main/java/com/tondeverton/demo/contactapi/entrypoints/rest/HealthController.java
+++ src/main/java/com/tondeverton/demo/contactapi/entrypoints/rest/HealthController.java
@@ -1,4 +1,4 @@
-package com.tondeverton.demo.contactapi.entrypoint.rest;
+package com.tondeverton.demo.contactapi.entrypoints.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
