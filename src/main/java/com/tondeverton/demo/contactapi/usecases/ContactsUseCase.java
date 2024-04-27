25c7087658f57536df64689ad14e663008e090c7
Everton Pereira <me@ton.dev.br>
2024-04-27 15:47:36 +0100

    Implement repository into usecase

--- src/main/java/com/tondeverton/demo/contactapi/usecases/ContactsUseCase.java
+++ src/main/java/com/tondeverton/demo/contactapi/usecases/ContactsUseCase.java
@@ -5,7 +5,9 @@ import com.tondeverton.demo.contactapi.repositories.Contact;
 import com.tondeverton.demo.contactapi.repositories.ContactRepository;
 import com.tondeverton.demo.contactapi.repositories.ContactToSave;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -27,19 +29,19 @@ public class ContactsUseCase {
         return contactRepository.add(dto);
     }
 
-    public Optional<Contact> update(@NotNull UUID id, @NotNull @Valid SaveContactRequest request) {
-        return null;
+    public Optional<Contact> update(@NotNull UUID id, @NotNull @Valid ContactToSave request) {
+        return contactRepository.update(id, request);
     }
 
     public Optional<Contact> get(@NotNull UUID id) {
-        return null;
+        return contactRepository.getById(id);
     }
 
-    public Page<Contact> getAll(String search, int page) {
+    public Page<Contact> getAll(@Length(max = 50) String search, @Max(30) int page) {
         return null;
     }
 
     public Optional<Boolean> delete(@NotNull UUID id) {
-        return Optional.empty();
+        return Optional.of(contactRepository.deleteById(id));
     }
 }
